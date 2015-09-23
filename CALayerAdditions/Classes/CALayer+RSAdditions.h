26abf51df9c91d415156cfe82351dc85fbc9bbd3
John Wong <huangxiaozhe1988@gmail.com>
2015-09-24 01:27:03 +0800

    Finished

---
+++ CALayerAdditions/Classes/CALayer+RSAdditions.h
@@ -0,0 +1,26 @@
+//
+//  CALayer+RSAdditions.h
+//  CALayerAdditions
+//
+//  Created by Josh Brown on 8/2/12.
+//  Copyright (c) 2012 Roadfire Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CALayer (RSAdditions)
+
+@property CGFloat top;
+@property CGFloat bottom;
+@property CGFloat left;
+@property CGFloat right;
+
+@property CGFloat centerX;
+@property CGFloat centerY;
+
+@property CGFloat height;
+@property CGFloat width;
+
+@property CGPoint center;
+
+@end
